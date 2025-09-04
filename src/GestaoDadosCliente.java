879817411db3bcb6769d11b49acea0a704846783
Igor Agiani <igor_011@hotmail.com.br>
2025-09-04 16:17:09 -0300

    adiciona 2 interfaces de cadastro e manipulação de dados

---
+++ src/GestaoDadosCliente.java
@@ -0,0 +1,6 @@
+public interface GestaoDadosCliente {
+    void cadastrarCliente(java.util.Scanner scanner);
+    void salvar(Cliente cliente);
+    void listarClientes();
+    void atualizarClientes();
+}
---
+++ src/GestaoDadosProduto.java
@@ -0,0 +1,6 @@
+public interface GestaoDadosProduto {
+    void cadastrarProduto(java.util.Scanner scanner);
+    void salvar(Produto produto);
+    void listarProdutos();
+    void atualizarProdutos();
+}
