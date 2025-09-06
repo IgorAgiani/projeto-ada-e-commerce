cabad39a7e766dc564d9865569119c177731bf87
Igor Agiani <igor_011@hotmail.com.br>
2025-09-06 06:58:14 -0300

    substitui interfaces duplicadas por GerenciadorCadastro

--- src/GestaoDadosProduto.java
+++
@@ -1,6 +0,0 @@
-public interface GestaoDadosProduto {
-    void cadastrarProduto(java.util.Scanner scanner);
-    void salvar(Produto produto);
-    void listarProdutos();
-    void atualizarProdutos();
-}
